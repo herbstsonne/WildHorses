85ba155d1e958c7e5fa6dd763116a976f47b6731
Miriam Pfaffenbach <miriam.pfaffenbach@wsa.com>
2024-09-09 22:21:41 +0200

    Initial Commit

---
+++ WildHorses/WildHorsesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildHorsesApp.swift
+//  WildHorses
+//
+//  Created by Miriam Pfaffenbach on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct WildHorsesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
